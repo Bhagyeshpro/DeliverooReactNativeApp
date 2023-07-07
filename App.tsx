47daf1b893b8b3a37aadbb3cdd40c1c445030712
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2023-07-07 18:35:55 +0530

    Nav

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#059599"}}>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>Dev</Text>
+      <Text style={{ fontSize: 40, fontWeight: "bold" }}>Navigation</Text>
     </View>
   )
 }
